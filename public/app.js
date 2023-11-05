1482fedc9e0fb72c2bb8f81eeca8397651711227
Jackury13 <143567012+Jackury13@users.noreply.github.com>
2023-11-05 08:39:41 -0800

    finally got the score counter working!

--- public/app.js
+++ public/app.js
@@ -1,28 +1,26 @@
 const wordDisplay = document.querySelector(".word-display")
-const guessesText = document.querySelector("guesses-text b")
-const keyboardDiv = document.querySelector(".keyboard")
+const livesLeft = document.querySelector(".points p")
+const keyboard = document.querySelector(".keyboard")
 
 
-
-
-let currentWord, wrongGuessCount = 0;
-const maxGuesses = 5;
-
-const getRandomWord = () => {
+let currentWord, wrongGuessCount = '0';
+const maxGuesses = 6;
+//selecting random word and hint from word-list.js file
+const getRandomWord = function() {
     const {word, hint} = wordList[Math.floor(Math.random() * wordList.length)];
     currentWord = word;
-    console.log (word);
+    console.log(word)
     document.querySelector(".hint-text p").innerText = hint;
     wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join("");
 }
 
-
-const initGame = (button, clickedLetter) => {
+    
+// function to see if the selected letter is in the random word.
+const initHorde = (_button, clickedLetter) => {
 
     if(currentWord.includes(clickedLetter)) {
-
         [...currentWord].forEach((letter, index) => {
-            if(letter == clickedLetter) {
+            if(letter === clickedLetter) {
                 wordDisplay.querySelectorAll("li")[index].innerText = letter;
                 wordDisplay.querySelectorAll("li")[index].classList.add("guessed"); 
             }
@@ -31,17 +29,17 @@ const initGame = (button, clickedLetter) => {
     } else {
         wrongGuessCount++;
     }
-    guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+    livesLeft.innerText = `${wrongGuessCount} / ${maxGuesses}`;
 }
-console.log(wrongGuessCount)
 //keyboard
-for(let i = 97; i<122; i++){
+for(let i = 97; i<122; i++) {
     const button = document.createElement("button");
-    button.innerText = String.fromCharCode(i);
-    keyboardDiv.appendChild(button);
-    button.addEventListener("click", e => initGame(e.target, String.fromCharCode(i)));
+    button.classList.add("letters")
+    button.innerText = String.fromCharCode(i)
+    keyboard.appendChild(button);
+    button.addEventListener("click", (e) => initHorde(e.target, String.fromCharCode(i)));
 }
-console.log(initGame)
+
 getRandomWord();
 
 
--- public/word-list.js
+++ public/word-list.js
@@ -1,16 +1,19 @@
 const wordList = [
     {
         word: "zombie",
-        hint: "The Walking Dead",
-    
+        hint: "The Walking Dead"
+    },
+    {
         word: "brains",
-        hint: "Zombies favorite food",
-    
+        hint: "Zombies favorite food"
+    },
+    {
         word: "virus",
-        hint: "What started the zombie plague",
-
+        hint: "What started the zombie plague"
+    },
+    {
         word: "double tap",
         hint: "rule #2 in the movie 'Zombieland'"
+    },
 
-    }
-    ]
+]
